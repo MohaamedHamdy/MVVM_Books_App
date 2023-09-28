36a52560ef370456c6869e255f079d5ee06e3cf0
MohaamedHamdy <m7mdhamdy45@gmail.com>
2023-09-29 02:38:42 +0300

    add screenutil package to main

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:mvvm_books/features/onBoarding/presetntation/view/on_board_screen.dart';
 
 void main() {
@@ -10,18 +11,25 @@ class BooksApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(
-        appBarTheme: const AppBarTheme(
-          elevation: 0.0,
-          backgroundColor: Colors.transparent,
-          iconTheme: IconThemeData(
-            color: Colors.black,
+    return ScreenUtilInit(
+      designSize: const Size(360, 690),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      builder: (context, child) {
+        return MaterialApp(
+          theme: ThemeData(
+            appBarTheme: const AppBarTheme(
+              elevation: 0.0,
+              backgroundColor: Colors.transparent,
+              iconTheme: IconThemeData(
+                color: Colors.black,
+              ),
+            ),
+            fontFamily: 'HK GROTESK',
           ),
-        ),
-        fontFamily: 'HK GROTESK',
-      ),
-      home: const OnBoardScreen(),
+          home: const OnBoardScreen(),
+        );
+      },
     );
   }
 }
