212bb3ed5cd379d64fceafc29d7f504b11538a94
MohaamedHamdy <m7mdhamdy45@gmail.com>
2023-09-29 02:37:24 +0300

    refactor code in onboarding app bar

--- lib/features/onBoarding/presetntation/view/on_board_screen.dart
+++ lib/features/onBoarding/presetntation/view/on_board_screen.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:mvvm_books/core/utils/styles.dart';
 
 import 'widgets/on_board_body.dart';
 
@@ -10,24 +8,7 @@ class OnBoardScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
-      // appBar: AppBar(
-      //   leading: IconButton(
-      //     onPressed: () {},
-      //     icon: const Opacity(
-      //       opacity: 0.8,
-      //       child: Icon(
-      //         FontAwesomeIcons.arrowLeftLong,
-      //       ),
-      //     ),
-      //   ),
-      //   actions: const [
-      //     Text(
-      //       'Skip',
-      //       style: Style.styles14,
-      //     ),
-      //   ],
-      // ),
-      body: OnBoardBody(),
+      body: SafeArea(child: OnBoardBody()),
     );
   }
 }
--- lib/features/onBoarding/presetntation/view/widgets/custom_app_bar.dart
+++ lib/features/onBoarding/presetntation/view/widgets/custom_app_bar.dart
@@ -9,7 +9,7 @@ class CustomAppBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 34),
+      padding: const EdgeInsets.symmetric(horizontal: 8.0),
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
