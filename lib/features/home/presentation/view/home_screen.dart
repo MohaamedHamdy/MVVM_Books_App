97c8e3dccbfe6688649fe3722197690fdfefe047
MohaamedHamdy <m7mdhamdy45@gmail.com>
2023-09-29 23:05:15 +0300

    change the whole home app bar code

--- lib/features/home/presentation/view/home_screen.dart
+++ lib/features/home/presentation/view/home_screen.dart
@@ -1,33 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:mvvm_books/core/utils/styles.dart';
+import 'widgets/home_screen_body.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      extendBodyBehindAppBar: true,
-      appBar: AppBar(
-        title: Text(
-          'Book of the week',
-          style: Style.styles20,
-        ),
-        centerTitle: true,
-        actions: [
-          Padding(
-            padding: const EdgeInsets.only(right: 8.0),
-            child: IconButton(
-              onPressed: () {},
-              icon: const Icon(
-                Icons.menu,
-                size: 36,
-              ),
-            ),
-          ),
-        ],
-      ),
-      // body: ,
+    return const Scaffold(
+      body: HomeScreenBody(),
     );
   }
 }
