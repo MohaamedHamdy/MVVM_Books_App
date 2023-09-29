d3994354677c592bfe4f4918ddf6c8a1631337e4
MohaamedHamdy <m7mdhamdy45@gmail.com>
2023-09-29 22:40:20 +0300

    home screenn app bar done

--- lib/features/home/presentation/view/home_screen.dart
+++ lib/features/home/presentation/view/home_screen.dart
@@ -1,10 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:mvvm_books/core/utils/styles.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      extendBodyBehindAppBar: true,
+      appBar: AppBar(
+        title: Text(
+          'Book of the week',
+          style: Style.styles20,
+        ),
+        centerTitle: true,
+        actions: [
+          Padding(
+            padding: const EdgeInsets.only(right: 8.0),
+            child: IconButton(
+              onPressed: () {},
+              icon: const Icon(
+                Icons.menu,
+                size: 36,
+              ),
+            ),
+          ),
+        ],
+      ),
+      // body: ,
+    );
   }
 }
