b2372f672456fb96664f15ea7e9bb7008504fe7b
MohaamedHamdy <m7mdhamdy45@gmail.com>
2023-09-29 23:09:00 +0300

    background vector in home screen added

---
+++ lib/features/home/presentation/view/widgets/custom_app_bar.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:mvvm_books/core/utils/styles.dart';
+
+class CustomAppBar extends StatelessWidget {
+  const CustomAppBar({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Positioned(
+      left: 8.w,
+      top: 43.h,
+      child: Row(
+        children: [
+          IconButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            icon: const Icon(Icons.arrow_back),
+          ),
+          SizedBox(
+            width: MediaQuery.of(context).size.width * 0.13,
+          ),
+          Text(
+            'Book of the week',
+            style: Style.styles20,
+          ),
+          SizedBox(
+            width: MediaQuery.of(context).size.width * 0.15,
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: Icon(
+              Icons.menu,
+              size: 32.sp,
+              color: Colors.black,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/features/home/presentation/view/widgets/home_screen_body.dart
+++ lib/features/home/presentation/view/widgets/home_screen_body.dart
@@ -1,64 +1,21 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:mvvm_books/core/utils/styles.dart';
+
+import 'package:mvvm_books/features/home/presentation/view/widgets/custom_app_bar.dart';
 
 class HomeScreenBody extends StatelessWidget {
   const HomeScreenBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    // return Container(
-    //   margin: EdgeInsets.only(bottom: 250.h),
-    //   alignment: Alignment.topCenter,
-    //   decoration: const BoxDecoration(
-    //     image: DecorationImage(
-    //       image: AssetImage('assets/images/Vector1.png'),
-    //     ),
-    //   ),
-    //   child: Padding(
-    //     padding: EdgeInsets.only(top: 100.h, left: 20.h, right: 20.h),
-    //     child: Column(
-    //       children: [],
-    //     ),
-    //   ),
-    // );
     return SizedBox(
       width: double.maxFinite,
       height: double.maxFinite,
       child: Stack(
         children: [
           Positioned(
-            left: 8.w,
-            top: 43.h,
-            child: Row(
-              children: [
-                IconButton(
-                  onPressed: () {
-                    Navigator.pop(context);
-                  },
-                  icon: const Icon(Icons.arrow_back),
-                ),
-                SizedBox(
-                  width: MediaQuery.of(context).size.width * 0.13,
-                ),
-                Text(
-                  'Book of the week',
-                  style: Style.styles20,
-                ),
-                SizedBox(
-                  width: MediaQuery.of(context).size.width * 0.15,
-                ),
-                IconButton(
-                  onPressed: () {},
-                  icon: const Icon(
-                    Icons.menu,
-                    size: 34,
-                    color: Colors.black,
-                  ),
-                ),
-              ],
-            ),
+            child: Image.asset('assets/images/Vector1.png'),
           ),
+          const CustomAppBar(),
         ],
       ),
     );
