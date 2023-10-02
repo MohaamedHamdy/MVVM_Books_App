043664543e9ff68c125f9f783d09e207f09d1361
MohaamedHamdy <m7mdhamdy45@gmail.com>
2023-10-02 20:21:14 +0300

    refactor platforms sign up code

--- lib/features/authentication/presentation/views/widgets/platform_sign_up.dart
+++ lib/features/authentication/presentation/views/widgets/platform_sign_up.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:mvvm_books/constants.dart';
 
 class PlatformsSignUpWidget extends StatelessWidget {
@@ -7,48 +8,35 @@ class PlatformsSignUpWidget extends StatelessWidget {
     super.key,
   });
 
+  static List icons = [
+    const Icon(
+      Icons.apple,
+    ),
+    const Icon(
+      Icons.facebook,
+    ),
+    const Icon(
+      FontAwesomeIcons.google,
+    ),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Row(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        CircleAvatar(
-          backgroundColor: kPrimaryColor,
-          child: IconButton(
-            onPressed: () {},
-            icon: const Icon(
-              Icons.apple_sharp,
-              color: Colors.white,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: List.generate(
+          icons.length,
+          (index) => Padding(
+            padding: EdgeInsets.only(right: 10.0.w),
+            child: CircleAvatar(
+              backgroundColor: kPrimaryColor,
+              child: IconButton(
+                onPressed: () {},
+                icon: icons[index],
+                color: Colors.white,
+              ),
             ),
           ),
-        ),
-        SizedBox(
-          width: 12.0.w,
-        ),
-        CircleAvatar(
-          backgroundColor: kPrimaryColor,
-          child: IconButton(
-            onPressed: () {},
-            icon: const Icon(
-              Icons.facebook,
-              color: Colors.white,
-            ),
-          ),
-        ),
-        SizedBox(
-          width: 12.0.w,
-        ),
-        CircleAvatar(
-          backgroundColor: kPrimaryColor,
-          child: IconButton(
-            onPressed: () {},
-            icon: const Icon(
-              Icons.apple_sharp,
-              color: Colors.white,
-            ),
-          ),
-        ),
-      ],
-    );
+        ));
   }
 }
