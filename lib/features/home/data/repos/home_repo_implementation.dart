1481cd4b545a0f9056f656939b2833375f5f388f
MohaamedHamdy <m7mdhamdy45@gmail.com>
2023-10-03 12:11:56 +0300

    implement fetch featuredbooks and book of the week

--- lib/features/home/data/repos/home_repo_implementation.dart
+++ lib/features/home/data/repos/home_repo_implementation.dart
@@ -1,19 +1,52 @@
+import 'dart:math';
+
 import 'package:dartz/dartz.dart';
 import 'package:mvvm_books/core/errors/failure.dart';
+import 'package:mvvm_books/core/utils/api_service.dart';
 import 'package:mvvm_books/features/home/data/models/book_model/book_model.dart';
 import 'package:mvvm_books/features/home/data/repos/home_repo.dart';
 
 class HomeRepoImpl implements HomeRepo {
+  final ApiService api;
+
+  HomeRepoImpl(this.api);
+
   @override
-  Future<Either<Failure, List<BookModel>>> fetchBookOfTheWeekBook() {
-    // TODO: implement fetchBookOfTheWeekBook
-    throw UnimplementedError();
+  Future<Either<Failure, List<BookModel>>> fetchBookOfTheWeekBook() async {
+    final random = Random();
+    try {
+      var allIds = await api.get(endPoints: 'volumes?q=subject:programming');
+      List ids = [];
+      List<BookModel> books = [];
+      for (var id in allIds['items']['id']) {
+        ids.add(id);
+      }
+      var randomId = random.nextInt(ids.length);
+
+      var data = await api.get(endPoints: 'volumes/:$randomId');
+
+      for (var item in data['items']) {
+        books.add(item);
+      }
+
+      return right(books);
+    } catch (e) {
+      return left(ServerFailure());
+    }
   }
 
   @override
-  Future<Either<Failure, List<BookModel>>> fetchFeaturedBooks() {
-    // TODO: implement fetchFeaturedBooks
-    throw UnimplementedError();
+  Future<Either<Failure, List<BookModel>>> fetchFeaturedBooks() async {
+    try {
+      var data = await api.get(endPoints: 'volumes?q=subject:programming');
+      List<BookModel> books = [];
+      for (var item in data['items']) {
+        books.add(item);
+      }
+      return right(books);
+    } catch (e) {
+      return left(ServerFailure());
+    }
   }
 
   @override
