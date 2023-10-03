1a1246c4380e0ea008030a3a82a319dda5fdc5cb
MohaamedHamdy <m7mdhamdy45@gmail.com>
2023-10-03 14:04:05 +0300

    create service locator and add get it package

---
+++ lib/core/utils/service_locator.dart
@@ -0,0 +1,21 @@
+import 'package:dio/dio.dart';
+import 'package:get_it/get_it.dart';
+import 'package:http/http.dart';
+import 'package:mvvm_books/core/utils/api_service.dart';
+import 'package:mvvm_books/features/home/data/repos/home_repo_implementation.dart';
+
+final getIt = GetIt.instance;
+
+void setup() {
+  getIt.registerSingleton<ApiService>(
+    ApiService(
+      dio: Dio(),
+    ),
+  );
+
+  getIt.registerSingleton<HomeRepoImpl>(
+    HomeRepoImpl(
+      getIt.get<ApiService>(),
+    ),
+  );
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:mvvm_books/core/utils/router.dart';
+import 'package:mvvm_books/core/utils/service_locator.dart';
+import 'package:mvvm_books/features/home/data/repos/home_repo_implementation.dart';
+import 'package:mvvm_books/features/home/presentation/view%20model/book%20of%20the%20week%20cubit/book_of_the_week_cubit.dart';
+import 'package:mvvm_books/features/home/presentation/view%20model/featured%20books%20cubit/featured_books_cubit.dart';
 
 void main() {
   runApp(const BooksApp());
@@ -16,19 +21,33 @@ class BooksApp extends StatelessWidget {
       minTextAdapt: true,
       splitScreenMode: true,
       builder: (context, child) {
-        return MaterialApp(
-          debugShowCheckedModeBanner: false,
-          theme: ThemeData(
-            appBarTheme: const AppBarTheme(
-              elevation: 0.0,
-              backgroundColor: Colors.transparent,
-              iconTheme: IconThemeData(
-                color: Colors.black,
+        return MultiBlocProvider(
+          providers: [
+            BlocProvider(
+              create: (_) => FeaturedBooksCubit(
+                getIt.get<HomeRepoImpl>(),
               ),
             ),
-            fontFamily: 'HK GROTESK',
+            BlocProvider(
+              create: (_) => BookOfTheWeekCubit(
+                getIt.get<HomeRepoImpl>(),
+              ),
+            ),
+          ],
+          child: MaterialApp(
+            debugShowCheckedModeBanner: false,
+            theme: ThemeData(
+              appBarTheme: const AppBarTheme(
+                elevation: 0.0,
+                backgroundColor: Colors.transparent,
+                iconTheme: IconThemeData(
+                  color: Colors.black,
+                ),
+              ),
+              fontFamily: 'HK GROTESK',
+            ),
+            onGenerateRoute: AppRouter.generateRoute,
           ),
-          onGenerateRoute: AppRouter.generateRoute,
         );
       },
     );
